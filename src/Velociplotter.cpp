c918be1a9028386727dfa85a1ca9ca2f62afd465
Alli Gilbreath <alligilbreath@Allis-MacBook-Pro.local>
2018-04-30 16:16:12 -0700

    Why is input 3 different?

--- src/Velociplotter.cpp
+++ src/Velociplotter.cpp
@@ -128,9 +128,9 @@ void Velociplotter::ReadInputsFromFile()
     cout << "Input file is: " << _inputFilePath << endl;
     for(unsigned int i = 0; i < _validPositions.size(); i++)
     {
-        cout << "Latitude: " << _validPositions.at(i).GetLatitude() << endl;
-        cout << "Longitude: " << _validPositions.at(i).GetLongitude() << endl;
-        cout << "Time: " << _validPositions.at(i).GetTime() << endl;
+//        cout << "Latitude: " << _validPositions.at(i).GetLatitude() << endl;
+//        cout << "Longitude: " << _validPositions.at(i).GetLongitude() << endl;
+//        cout << "Time: " << _validPositions.at(i).GetTime() << endl;
     }
     
     }
@@ -149,11 +149,11 @@ void Velociplotter::CalculateAverageVelocities(){
     double currVelocity = 0;
     double distance = 0;
     unsigned long timeDiff = 0;
-    unsigned int index = 0;
+    unsigned int index = 1;
     unsigned long totalAmountOfTime = _validPositions.back().GetTime() - _validPositions.front().GetTime();
-    for(unsigned long i = 1; (i < totalAmountOfTime) && (index < _validPositions.size() - 1); i++)
+    for(unsigned long i = 1; (i < totalAmountOfTime) && (index < _validPositions.size()); i++)
     {
-        timeDiff = _validPositions.at(index + 1).GetTime() - _validPositions.at(index).GetTime();
+        timeDiff = _validPositions.at(index).GetTime() - _validPositions.at(index - 1).GetTime();
         if(timeDiff != 1) //&& _velocities.size() != 0)
         {
             currVelocity = distance / timeDiff;
@@ -166,7 +166,7 @@ void Velociplotter::CalculateAverageVelocities(){
         }
         else
         {
-            distance = _validPositions.at(index).CalcDistanceKmTo(_validPositions.at(index + 1));
+            distance = _validPositions.at(index - 1).CalcDistanceKmTo(_validPositions.at(index));
             currVelocity = distance / timeDiff;
             _velocities.push_back(currVelocity);
         }
@@ -175,7 +175,7 @@ void Velociplotter::CalculateAverageVelocities(){
     
     for(unsigned int i = 0; i < _velocities.size(); i++)
     {
-        cout << "Velocity is: " << _velocities.at(i) << endl;
+        //cout << "Velocity is: " << _velocities.at(i) << endl;
     }
     
 }
@@ -189,7 +189,33 @@ void Velociplotter::WriteOutputsToFile(){
     //Even if a GPS sentence does not exist for a specified second, you must fill in the missing data.
     //The speed at the time of the first GPS reading from the input GPS log cannot be determined as there is no previous GPS location information.
     //Thus, the first output for the GPS plot data file should start one second after the time of the first GPS reading in the input log file.
-        
+    ofstream outputFile;
+    cout << "Opening file" << _outputFilePath << endl;
+    outputFile.open(_outputFilePath);
+    if (!outputFile.is_open())//check if file is open
+    {
+        cout << "Could not open file: " << _outputFilePath << endl;
+    }
+    else
+    {
+        if(_velocities.size() == 0 || _validPositions.size() == 0)
+        {
+            cout << "Velocities has size: " << _velocities.size() << " and validPositions has size: " << _validPositions.size() << endl;
+            outputFile.close();
+            return;
+        }
+        else
+        {
+            cout << _validPositions.back().GetTime() << endl;
+            unsigned long firstTimeVal = _validPositions.front().GetTime() + 1;
+            for(unsigned int i = 0; i < _velocities.size(); i++)
+            {
+                unsigned long newTime = firstTimeVal + i;
+                outputFile << newTime << " " << _velocities.at(i) << endl;
+            }
+        }
+    }
+    outputFile.close();
         
         
         
